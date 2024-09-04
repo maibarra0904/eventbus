28120238cd7bd55e8d6f82e48be0683f377779ae
Mario Ibarra <mario.ibarra.86@gmail.com>
2024-09-03 23:40:27 -0500

    Cambio a url kubernete creado

--- src/index.ts
+++ src/index.ts
@@ -18,15 +18,15 @@ app.post('/events', async(req, res) => {
 
     events.push(event)
 
-    await axios.post('http://localhost:4000/events', event)
-    await axios.post('http://localhost:4001/events', event)
-    try {
-      await axios.post('http://localhost:4002/events', event)  
-    } catch (error) {
-      console.log(error)
-    }
+    await axios.post('http://posts-clusterip-srv:4000/events', event)
+    // await axios.post('http://localhost:4001/events', event)
+    // try {
+    //   await axios.post('http://localhost:4002/events', event)  
+    // } catch (error) {
+    //   console.log(error)
+    // }
     
-    await axios.post('http://localhost:4003/events', event)
+    // await axios.post('http://localhost:4003/events', event)
 
     res.status(201).send({status: "Ok"});
 });
